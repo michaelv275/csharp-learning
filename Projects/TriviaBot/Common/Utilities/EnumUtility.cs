e4331f3f083392d5d626daed1dcc7b207ae06d05
Michael Vanantwerp <michaelv275@gmail.com>
2025-12-10 16:30:35 -0600

    Create EnumUtility.cs

---
+++ Projects/TriviaBot/Common/Utilities/EnumUtility.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace TriviaBot.Common.Utilities
+{
+    public static class EnumUtility
+    {
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+
+            if (fi.GetCustomAttributes(typeof(DescriptionAttribute), false) is DescriptionAttribute[] attributes && attributes.Any())
+            {
+                return attributes.First().Description;
+            }
+
+            return value.ToString();
+        }
+
+        public static IEnumerable<T> GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+
+        public static string GetEnumMemberValue<T>(this T value) where T : Enum
+        {
+            return typeof(T)
+                .GetTypeInfo()
+                .DeclaredMembers
+                .SingleOrDefault(x => x.Name == value.ToString())
+                ?.GetCustomAttribute<EnumMemberAttribute>(false)
+                ?.Value;
+        }
+    }
+}
