75d5a73f530687764b58a89fce46d87698a1ef59
Michael Vanantwerp <michaelv275@gmail.com>
2026-02-18 14:48:50 -0600

    Add Console obfuscation

--- Projects/TriviaBot/Common/Utilities/ConsoleUtility.cs
+++ Projects/TriviaBot/Common/Utilities/ConsoleUtility.cs
@@ -1,4 +1,6 @@
-﻿namespace TriviaBot.Common.Utilities
+﻿using System.Text;
+
+namespace TriviaBot.Common.Utilities
 {
     public static class ConsoleUtility
     {
@@ -16,6 +18,37 @@
             Console.ForegroundColor = color;
             Console.Write(message);
             Console.ForegroundColor = previousColor;
+        }
+
+        public static string GetSecureUserInput()
+        {
+            StringBuilder passwordBuilder = new StringBuilder();
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true); // Intercept the key press
+
+                // Handle standard characters and the Backspace key
+                if (char.IsControl(key.KeyChar))
+                {
+                    if (key.Key == ConsoleKey.Backspace && passwordBuilder.Length > 0)
+                    {
+                        _ = passwordBuilder.Remove(passwordBuilder.Length - 1, 1);
+                        Console.Write("\b \b"); // Erase the character and the asterisk
+                    }
+                }
+                else
+                {
+                    _ = passwordBuilder.Append(key.KeyChar);
+                    Console.Write("*"); // Display an asterisk for the character
+                }
+            }
+            while (key.Key != ConsoleKey.Enter); // Stop when the Enter key is pressed
+
+            // Remove the newline character added by pressing Enter from the console output
+            Console.WriteLine();
+            return passwordBuilder.ToString();
         }
     }
 }
